42b926b89aef392c3aff064ad95b173eb3248cab
Hector Hernandez <hectortamriel@gmail.com>
2020-05-08 23:12:35 -0700

    new files

---
+++ script.js
@@ -0,0 +1,19 @@
+var firstName='John';
+var hobby='athlete';
+
+switch (hobby){
+    case 'athlete':
+        console.log(firstName + ' plays football with his friends!');
+        break;
+    case 'gamer':
+        console.log(firstName + ' plaays videogames!');
+        break;
+    case 'painter':
+        console.log(firstName + ' is a painter!');
+        break;
+    default:
+        console.log(firstName + ' does something else!');
+    }
+
+
+ 
